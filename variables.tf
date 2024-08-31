42534042152acf7700e90224c184f9ef514b7b7b
Luciano <lucianodefranca@outlook.com>
2024-08-31 17:47:41 -0300

    feat: initial commit

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "region" { 
+}
+
+variable "project_name" {
+}
+
+variable "ssm_vpc_id" {
+  
+}
+
+variable "ssm_pub_subnet_1" {
+  
+}
+
+variable "ssm_pub_subnet_2" {
+  
+}
+
+variable "ssm_pub_subnet_3" {
+  
+}
+
+
+variable "ssm_priv_subnet_1" {
+  
+}
+
+variable "ssm_priv_subnet_2" {
+  
+}
+
+variable "ssm_priv_subnet_3" {
+  
+}
